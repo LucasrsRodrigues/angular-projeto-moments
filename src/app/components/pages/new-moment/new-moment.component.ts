ca3b3f8acbd8866ba454a1afb000c3e0f6b64998
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2022-12-23 16:19:22 -0300

    finish about

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Moment } from 'src/app/interfaces/Moments';
+import { MessagesService } from 'src/app/service/messages.service';
+import { MomentService } from 'src/app/service/moment.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-moment',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText = 'Compartilhar!';
+
+  constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router) { }
+
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add("Momento adicionado com sucesso!");
+
+    this.router.navigate(['/']);
+
+  }
 }
